6efe46b1a6dd4b05a9b124259f474081dddd6d42
Orestis.Mark <orestis.markozanes@gmail.com>
2017-09-23 20:55:26 -0400

    minor changes

--- RatTrackingApp/app/src/main/java/team42/cs2340/rattrackingapp/MainActivity.java
+++ RatTrackingApp/app/src/main/java/team42/cs2340/rattrackingapp/MainActivity.java
@@ -2,6 +2,8 @@ package team42.cs2340.rattrackingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button logIn = (Button) findViewById(R.id.button);
+        logIn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
+
+        final Button register = (Button) findViewById(R.id.button);
+        register.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
     }
 }
