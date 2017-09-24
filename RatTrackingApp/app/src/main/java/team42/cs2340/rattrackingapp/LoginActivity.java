216c6a65ee39ce8424825d53574d4f03cc31de2a
J <jcnguyen1994@gmail.com>
2017-09-24 12:21:01 -0400

    Login Button working.

---
+++ RatTrackingApp/app/src/main/java/team42/cs2340/rattrackingapp/LoginActivity.java
@@ -0,0 +1,15 @@
+package team42.cs2340.rattrackingapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by King Jay on 9/24/2017.
+ */
+
+public class LoginActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
--- RatTrackingApp/app/src/main/java/team42/cs2340/rattrackingapp/MainActivity.java
+++ RatTrackingApp/app/src/main/java/team42/cs2340/rattrackingapp/MainActivity.java
@@ -1,5 +1,6 @@
 package team42.cs2340.rattrackingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,18 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button logIn = (Button) findViewById(R.id.button);
-        logIn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Code here executes on main thread after user presses button
-            }
-        });
+    }
 
-        final Button register = (Button) findViewById(R.id.button2);
-        register.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Code here executes on main thread after user presses button
-            }
-        });
+    public void onLoginClick(View v) {
+        if (v.getId() == R.id.bLogin) {
+            Intent i = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(i);
+        }
     }
 }
