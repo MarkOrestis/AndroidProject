f82984fee6e36804c734a25663bf05994822754b
Orestis.Mark <orestis.markozanes@gmail.com>
2017-09-23 20:12:29 -0400

    Added buttons but no functionality, checking stuff

--- RatTrackingApp/app/src/main/java/com/example/cs2340/rattrackingapp/MainActivity.java
+++ RatTrackingApp/app/src/main/java/com/example/cs2340/rattrackingapp/MainActivity.java
@@ -1,7 +1,11 @@
-package com.example.orest.rattrackingapp;
+package com.example.cs2340.rattrackingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.orest.rattrackingapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button logIn = (Button) findViewById(R.id.button);
+        logIn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //code should execute here
+            }
+
+            });
+
+        final Button register = (Button) findViewById(R.id.button2);
+        register.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //code should execute here
+            }
+
+        });
     }
+
 }
