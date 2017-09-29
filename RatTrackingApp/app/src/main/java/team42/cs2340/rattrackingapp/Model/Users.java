368a370b031525c513d6c70515e24b19604d58a3
King Jay <jcnguyen1994@gmail.com>
2017-09-29 12:26:07 -0400

    Changed editText id's

--- RatTrackingApp/app/src/main/java/team42/cs2340/rattrackingapp/Model/Users.java
+++ RatTrackingApp/app/src/main/java/team42/cs2340/rattrackingapp/Model/Users.java
@@ -5,8 +5,13 @@ import java.util.ArrayList;
 /**
  * Created by King L0sr on 9/29/2017.
  */
-
 public class Users {
-    ArrayList[] database = new ArrayList[3];
+    private String email;
+    private String password;
 
+    public Users(String password, String email) {
+        this.password=password;
+        this.email=email;
+    }
 }
+
