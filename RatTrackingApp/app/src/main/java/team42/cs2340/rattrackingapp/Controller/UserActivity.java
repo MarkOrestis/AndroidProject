ce0c55706c002375bbdc56752fe99ddad4ee596c
King Jay <jcnguyen1994@gmail.com>
2017-09-29 13:13:18 -0400

    I'm very confused.

--- RatTrackingApp/app/src/main/java/team42/cs2340/rattrackingapp/Controller/UserActivity.java
+++ RatTrackingApp/app/src/main/java/team42/cs2340/rattrackingapp/Controller/UserActivity.java
@@ -4,12 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-<<<<<<< HEAD
 import android.widget.EditText;
 import android.widget.RadioGroup;
-=======
 import android.widget.RadioButton;
->>>>>>> 5f52ae6049c2fa94db3282440df1c453cb27681c
 import team42.cs2340.rattrackingapp.Model.Users;
 import team42.cs2340.rattrackingapp.R;
 
@@ -30,25 +27,21 @@ public class UserActivity extends Activity {
             startActivity(i);
         }
     }
-<<<<<<< HEAD
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.rbUser:
+                if (checked)
+                    break;
+            case R.id.rbAdmin:
+                if (checked)
+                    // Ninjas rule
+                    break;
+        }
+    }
     public void onConfirmClick(View v) {
         EditText username = (EditText)findViewById(R.id.usernameUser);
         EditText email = (EditText)findViewById(R.id.emailUser);
@@ -59,23 +52,6 @@ public class UserActivity extends Activity {
         String usernameString = username.getText().toString();
         String emailString = email.getText().toString();
         String passwordString = password.getText().toString();
-        Users  = new Users(passwordString, emailString, userType);
-=======
-    public void onRadioButtonClicked(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
-
-        // Check which radio button was clicked
-        switch(view.getId()) {
-            case R.id.rbUser:
-                if (checked)
-                    // Pirates are the best
-                    break;
-            case R.id.rbAdmin:
-                if (checked)
-                    // Ninjas rule
-                    break;
-        }
->>>>>>> 5f52ae6049c2fa94db3282440df1c453cb27681c
+        Users  blabla = new Users(passwordString, emailString, userType);
     }
 }
