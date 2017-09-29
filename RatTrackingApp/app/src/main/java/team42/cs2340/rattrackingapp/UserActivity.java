96b26299539bb7e98eff68eab9272a99bea8a2fb
Orestis.Mark <orestis.markozanes@gmail.com>
2017-09-29 12:04:31 -0400

    cancel button for registration page

--- RatTrackingApp/app/src/main/java/team42/cs2340/rattrackingapp/UserActivity.java
+++ RatTrackingApp/app/src/main/java/team42/cs2340/rattrackingapp/UserActivity.java
@@ -1,7 +1,9 @@
 package team42.cs2340.rattrackingapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Orestis Markozanes on 9/27/2017.
@@ -13,5 +15,11 @@ public class UserActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+    }
+    public void onCancelUserClick(View v) {
+        if (v.getId() == R.id.bCancelUser) {
+            Intent i = new Intent(UserActivity.this, MainActivity.class);
+            startActivity(i);
+        }
     }
 }
