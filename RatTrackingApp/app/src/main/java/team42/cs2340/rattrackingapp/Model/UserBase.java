ba4e0195665470560f05b69403cc1c921f1a5f19
J <jcnguyen1994@gmail.com>
2017-10-01 20:22:35 -0400

    Changed UserBase to singleton

--- RatTrackingApp/app/src/main/java/team42/cs2340/rattrackingapp/Model/UserBase.java
+++ RatTrackingApp/app/src/main/java/team42/cs2340/rattrackingapp/Model/UserBase.java
@@ -1,13 +1,15 @@
 package team42.cs2340.rattrackingapp.Model;
 
+import android.app.Application;
+
 import java.util.ArrayList;
 
 /**
  * Created by King Jay on 10/1/2017.
  */
 
-public class UserBase {
-    private static ArrayList<Users> users;
+public class UserBase extends Application{
+    private ArrayList<Users> users;
     public UserBase() {
 
     }
