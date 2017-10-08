6aa070720cae406491155e51abce7aac6bd51131
J <jcnguyen1994@gmail.com>
2017-10-08 13:45:03 -0400

    Model class for data

---
+++ RatTrackingApp/app/src/main/java/team42/cs2340/rattrackingapp/Model/RatSightingData.java
@@ -0,0 +1,17 @@
+package team42.cs2340.rattrackingapp.Model;
+
+/**
+ * Created by King Jay on 10/8/2017.
+ */
+
+public class RatSightingData {
+    private String uniqueKey;
+    private String createdDate;
+    private String locationType;
+    private String incidentZip;
+    private String incidentAddress;
+    private String city;
+    private String borough;
+    private String latitude;
+    private String longitude;
+}
