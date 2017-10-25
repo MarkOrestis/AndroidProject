bbd85a89c3d79ea913dc538245beb69b33e351ec
Beatrice Domingo <patriciabdomingo@gmail.com>
2017-10-25 11:16:52 -0400

    message

--- RatTrackingApp/app/src/main/java/team42/cs2340/rattrackingapp/Model/Borough.java
+++ RatTrackingApp/app/src/main/java/team42/cs2340/rattrackingapp/Model/Borough.java
@@ -1,8 +1,34 @@
 package team42.cs2340.rattrackingapp.Model;
 
+
 /**
- * Created by Beatrice on 10/25/17.
+ * Created by callydecherd on 10/20/17.
  */
 
-public class Borough {
+public enum Borough {
+    MANHATTAN ("Manhattan"),
+    STATEN_ISLAND ("Staten Island"),
+    QUEENS ("Queens"),
+    BROOKLYN ("Brooklyn"),
+    BRONX ("Bronx");
+
+    /** the full string representation of the borough name */
+    private final String borough;
+
+
+    /**
+     * Constructor for the enumeration
+     *
+     * @param borough   full name of the course
+     */
+    Borough(String borough) {
+        this.borough = borough;
+    }
+
+
+    /**
+     *
+     * @return the display string representation of the course
+     */
+    public String toString() { return borough; }
 }
